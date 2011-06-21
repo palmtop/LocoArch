d29a0952334e5c2f4fca5e79c2b28b8cf41ee4d2
László Csilling <laco@csilling.com>
2011-06-21 12:33:25 -0700

    Actual status

--- src/LocoArch.java
+++ src/LocoArch.java
@@ -1,13 +1,25 @@
+import org.apache.commons.cli.*;
 
-public class LocoArch {
+public class LocoArch
+	{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+		{
+		defineOptions();
+
 		System.out.print("LocoArch");
 
-	}
+		}
 
-}
+	private static void defineOptions()
+	{
+		Option option1 = new Option("c","config-file",true,"path to the LocoArch configuration file");
+		option1.setArgs(1);option1.setArgName("filename");option1.setRequired(true);
+		Option option2 = new Option("o","operation",false,"operation: archive, list, restore, restorebyhash, checkintegrity, recover, freespace");
+		option2.setRequired(true);
+
+	}
+	}
