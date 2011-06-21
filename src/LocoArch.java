53e3ddf2a394b7d6db69f912d0dc1195d532e1fd
László Csilling <laco@csilling.com>
2011-06-21 11:34:11 -0700

    First real java file

---
+++ src/LocoArch.java
@@ -0,0 +1,13 @@
+
+public class LocoArch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("LocoArch");
+
+	}
+
+}
