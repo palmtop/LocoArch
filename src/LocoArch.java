e6000543d4e1df3e1384d263bd570f57e9dde038
László Csilling <laco@csilling.com>
2011-06-23 12:54:28 -0700

    new version

--- src/LocoArch.java
+++ src/LocoArch.java
@@ -8,7 +8,7 @@ public class LocoArch
 	 */
 	public static void main(String[] args)
 		{
-		defineOptions();
+		Options();
 
 		System.out.print("LocoArch");
 
@@ -18,8 +18,12 @@ public class LocoArch
 	{
 		Option option1 = new Option("c","config-file",true,"path to the LocoArch configuration file");
 		option1.setArgs(1);option1.setArgName("filename");option1.setRequired(true);
-		Option option2 = new Option("o","operation",false,"operation: archive, list, restore, restorebyhash, checkintegrity, recover, freespace");
-		option2.setRequired(true);
+		Option option2 = new Option("r","redundancy-filename",true,"path to the redundancy file during archiving");
+		option2.setArgName("filename");
+		Option option3 = new Option("d","archive-date", true, "archivation date during restore");
+		option3.setArgName("archivation date");
+		
+
 
 	}
 	}
