787b6a40b3b539dc3775bd4dcdadd2071702b640
László Csilling <laco@csilling.com>
2011-10-16 10:43:41 -0700

    Parameter checking started, added usage to a separate resource file.

--- src/LocoArch.java
+++ src/LocoArch.java
@@ -1,17 +1,105 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.commons.cli.*;
 
 public class LocoArch
 	{
+	public enum enumCommands
+	{
+	archive,
+	list,
+	restore,
+	checkintegrity,
+	recover,
+	freespace,
+	restorebyhash
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 		{
-		Options();
+		Options(args);
 
 		System.out.print("LocoArch");
 
+		}
+
+	private static void Options(String[] args)
+		{
+		// Process the options and select the program part to run
+		if (args.length==0)
+			{
+			printUsage("Not enough parameters");
+			}
+		else
+			{
+			enumCommands command = enumCommands.valueOf(args[0]);
+
+			switch (command) 
+			            {
+			            case archive: doArchive(); break;
+			            case list:
+			            case restore:
+			            case checkintegrity:
+			            case recover:
+			            case freespace:
+			            case restorebyhash:
+			            	printUsage("Command not implemented");break;
+			            default:
+			            	printUsage("Command not recognised"); break;
+			            				
+			            }
+			
+			}
+		
+		
+		}
+
+	private static void doArchive()
+		{
+		// TODO Auto-generated method stub
+		
+		}
+
+	public static void printUsage(String string)
+		{
+		// Print usage information
+		Class c=null;
+		try {
+			c = Class.forName("LocoArch");
+			} 
+		catch (Exception ex) 
+			{
+			// This should not happen.
+			}
+		InputStream s = c.getResourceAsStream("/resource/Usage.txt");
+		// do something with it.
+		int ch=-1;
+		try
+			{
+			ch = s.read();
+			} 
+		catch (IOException e)
+			{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+		while (ch!=-1)
+			{
+			System.out.print((char)ch);
+			try
+				{
+				ch = s.read();
+				} 
+			catch (IOException e)
+				{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+			}
 		}
 
 	private static void defineOptions()
